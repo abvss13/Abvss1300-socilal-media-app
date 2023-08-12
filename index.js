4f6f1088c4d4e66bb4b7097013eb5e7307dfebaf
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-12 20:20:14 +0300

    1

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ postForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const content = postContent.value.trim();
     if (content !== "") {
-        createPost(content);
+        createTweet(content);
         postContent.value = "";
     }
 });
 
-function createPost(content) {
-    const postElement = document.createElement("div");
-    postElement.classList.add("post");
-    postElement.innerHTML = `
-        <p class="post-content">${content}</p>
+function createTweet(content) {
+    const tweetElement = document.createElement("div");
+    tweetElement.classList.add("tweet");
+    tweetElement.innerHTML = `
+        <p class="tweet-content">${content}</p>
+        <div class="tweet-meta">
+            <span class="tweet-date">${formatDate(new Date())}</span>
+        </div>
     `;
-    postList.prepend(postElement);
+    postList.prepend(tweetElement);
+}
+
+function formatDate(date) {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
 }
